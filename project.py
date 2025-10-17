18dfa6727b631cca9a06cab7c7a4b377194d9b7d
kalonzo268-rgb <k.alonzo268@hotmail.com>
2025-10-17 12:39:34 -0600

    Create project.py

---
+++ project.py
@@ -0,0 +1,54 @@
+# 🤩✨⭐️ STAR 🌟💫🌠
+SolSySta = [
+            "Solis"
+]
+
+# 🪐 PLANETS 🌍
+VnPlnt = [
+          "Venus"
+]
+VnQStl = [
+          "Zoozve"
+]
+Vns = [
+       VnPlnt
+       +
+       VnQStl
+]
+SolSyPlnt = [
+             Vns
+]
+
+# 🐯🤢 BELT 🤫🤐
+PlutoPlnt = [
+             "Pluto"
+]
+PlutoMoons = [
+              "Charon"
+]
+Pluto = [
+         PlutoPlnt
+         +
+         PlutoMoons
+]
+Kuiper = [
+          Pluto
+]
+SolSyBelt = [
+             Kuiper
+]
+
+# 🚕🚖🥬🍷🧑‍✈️👩‍✈️👨‍✈️🗄️🗃️🚠🚡💩🌵🏜️☕️🍽️🍵🩻🇹🇨🎂 CLOUD 🍰🧁🍥🥮🧉🧮🆑🛄🔮🗜️👏🎬🐕🐄🎹🎻🏛️⏰🌁
+SolSyCloud = [
+              "Oort"
+]
+
+# 🛌🛏️😭🥺😢😔☹️😞😕🙁😥😪😓😟🤕🤒😿👿🎭🙍🙍‍♀️🙍‍♂️🫤💼🎒🐯🦁🐘🐅🦘🐊🦓🦏🦛🐆😷🧯👷👷‍♀️👷‍♂️🥽🧷🦺🫆☣️☢️🛟👩‍🔬🧑‍🔬👨‍🔬⛑️♐️🌵⛵️🎅🍶🍣🥗🫜 SOLAR SYSTEM 🤣😭😊😁😉😀😳☺️😆😄😃😞🥵🤦🤦‍♀️🤦‍♂️😹😂🥲🥹🛁🧼🧽🫧🧴🍜⚽️🥅🏟️🧦🥤🧂🛋️🍦🥎🧑‍💻👩‍💻👨‍💻👽🧑‍🏭👩‍🏭👨‍🏭😢😔☹️😕😕🙁😪😿🎤🎸
+SolSy = [
+         SolSySta 
+         + 
+         SolSyPlnt
+         +
+         SolSyBelt
+         +
+         SolSyCloud
